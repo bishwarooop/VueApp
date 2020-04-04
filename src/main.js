e30fbb1d8800402a35ca5b279391e00f2d4a61b3
Bishwaroop Basu <bishwaroop_b@hotmail.com>
2020-04-04 16:46:23 +0530

    vue app

--- src/main.js
+++ src/main.js
@@ -1,8 +1,34 @@
 import Vue from 'vue'
 import App from './App.vue'
+import API, { graphqlOperation } from '@aws-amplify/api'
+import PubSub from '@aws-amplify/pubsub';
+import {createTodo} from './graphql/mutations'
+import awsconfig from './aws-exports'
+
+
 
 Vue.config.productionTip = false
+API.configure(awsconfig);
+PubSub.configure(awsconfig);
+
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+async function createTodo() {
+
+const todo = { name: "Use AppSync", description: "Realtime and offline"}
+return await API.graphql(graphqlOperation(createTodo, {input: todo}))
+
+}
+
+const MutationButton = document.getElementById('MutationEventButton');
+const MutationResult = document.getElementById('MutationResult');
+
+MutationButton.addEventListener('click', (evt) => {
+  MutationResult.innerHTML = `MUTATION RESULTS:`;
+  createNewTodo().then( (evt) => {
+    MutationResult.innerHTML += `<p>${evt.data.createTodo.name} - ${evt.data.createTodo.description}</p>`
+  })
+});
